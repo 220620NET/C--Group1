5d9bac02282b1fba76872439e31d7812a577095a
Jay Sprout <jotasprout@gmail.com>
2022-06-21 23:38:05 -0400

    c sharp program

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿// The app asks the user to enter a number.
+
+Console.WriteLine("Pick a number, please.");
+string pickedNumber = Console.ReadLine();
+
+// THE STRING MUST BE CONVERTED TO AN INT AT SOME POINT
+
+int newNum = Convert.ToInt32(pickedNumber);
+
+int theNum = newNum;
+
+for(int i = 0; i < theNum; i++)
+{
+    // newNum increases by 1 each iteration after the first
+   int finalNum = i;
+
+   // The app will then print every number from 1 to whatever the user entered ...
+   // Except if the number is divisible by 3, it prints 'fizz' instead ...
+   if (finalNum % 3 == 0) 
+   {
+    Console.WriteLine("fizz ");
+   }
+   // OR if the number is divisible by 5, it prints 'buzz' instead
+    else if (finalNum % 5 == 0) 
+    {
+        Console.WriteLine("buzz ");
+    }
+
+    // OR if the number is divisible by 3 and 5, it prints 'fizzbuzz' instead 
+
+    else if (finalNum % 5 == 0 && finalNum % 3 == 0) 
+    {
+        Console.WriteLine("fizzbuzz ");
+    }
+
+    // if none of those conditions are met, this is where it prints the regular number
+
+    else
+    {
+        Console.WriteLine(finalNum);
+    }
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # C--Group1
 FizzBuzz with Autumn, Kabiru, and Collins
+
+## My Summary of Assignment - A Lazy User Story
+Here is my summary of the FizzBuzz instructions on lines 113-128 in Program.cs (I'll copy Kabiru on this):
+
+- The app asks the user to enter a number.
+- The app will then print every number from 1 to whatever the user entered, EXCEPT
+    - If the number is divisible by 3, it prints 'fizz' instead OR
+    - If the number is divisible by 5, it prints 'buzz' instead OR
+    - If the number is divisible by 3 and 5, it prints 'fizzbuzz' instead 
