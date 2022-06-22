7116627fc5a6024e8d59740bbb0ef25021083054
blackgbemi <global-blackgbemi@gmail.com>
2022-06-22 15:03:55 -0700

    code commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Pick a number, please");
+int endingNumber = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= endingNumber; i++)
+{
+	if (i % 3 == 0 && i % 5 == 0) {
+   Console.WriteLine("fizzbuzz"); 
+   }
+   else if (i % 5 == 0) {
+   	Console.WriteLine("buzz"); 
+   }
+   else if (i % 3 == 0) {
+   	Console.WriteLine("fizz");
+   }
+
+   else {
+    Console.WriteLine(i);	
+  }
+}
